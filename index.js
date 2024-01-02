3832a91679ac5cab0b79fc1d06e8ff66cb03e932
lighningLordZeus <kekelidzenika99@gmail.com>
2024-01-03 02:21:04 +0400

    fixed some bugs

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const display = document.getElementById("display");
 
+let lastInputIsOperator = false;
+
 function appendToDisplay(input) {
-    display.value += input;
-};
+  const operators = ["+", "-", "*", "/"];
+
+  if (
+    lastInputIsOperator &&
+    operators.includes(input)
+  ) {
+
+    display.value = display.value.slice(0, -1) + input;
+  } else {
+    const lastChar = display.value.slice(-1);
+    
+    if (lastChar === "." && input === ".") {
+      return;
+    }
+
+    if (
+      (display.value === "0" &&
+        input !== "." &&
+        !operators.includes(input)) || display.value === "error"
+    ) {
+      display.value = input;
+    } else {
+      display.value += input;
+    }
+  }
+
+  lastInputIsOperator = operators.includes(input);
+}
 
 function clearDisplay() {
-    display.value = "";
-};
+  display.value = "0";
+  lastInputIsOperator = false;
+}
 
 function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch(error) {
-        display.value = "error"
-    }
-};
+  try {
+    display.value = eval(display.value);
+    lastInputIsOperator = false;
+  } catch (error) {
+    display.value = "error";
+    lastInputIsOperator = false;
+  }
+}
